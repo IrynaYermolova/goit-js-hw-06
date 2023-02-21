3ee249d33cf31cfff785ebaae6030517ce3a301c
IrynaYermolova <ermolova_ira@ukr.net>
2023-02-21 12:16:35 +0200

    update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,16 @@
-
-
 const textInput = document.querySelector("#validation-input");
 
-textInput.addEventListener("blur", () => {
+textInput.addEventListener("blur", (event) => {
   const inputValue = textInput.value;
   const dataLength = textInput.getAttribute("data-length");
 
   if (inputValue.length === Number(dataLength)) {
     textInput.classList.remove("invalid");
     textInput.classList.add("valid");
+  } else {
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid");
   }
-  textInput.classList.remove("valid");
-  textInput.classList.add("invalid");
 });
-// console.log(textInput);
+console.log(textInput);
+
